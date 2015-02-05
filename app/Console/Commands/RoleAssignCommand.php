301e1946e98882c7c9290a481cf5fec1b411d5ef
Taskforcedev <taskforce2eu@gmail.com>
2015-02-05 12:13:48 +0000

    Completed functionality to add users to a role via command line.

--- app/Console/Commands/RoleAssignCommand.php
+++ app/Console/Commands/RoleAssignCommand.php
@@ -2,10 +2,13 @@
 
 namespace DiabloDB\Console\Commands;
 
+use Exception;
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use DiabloDB\User;
+use DiabloDB\Role;
+use DiabloDB\RoleMember;
 
 class RoleAssignCommand extends Command
 {
@@ -40,12 +43,27 @@ class RoleAssignCommand extends Command
      */
     public function fire()
     {
-        /* Get list of users */
-        $users = User::all();
-        $userData = $this->buildUserTable($users);
-        $this->table(['id', 'username'], $userData);
+        try {
+            $user_id = $this->getUserId();
+        } catch (Exception $e) {
+            return $this->line("Error user specified is invalid");
+        }
+
+        try {
+            $role_id = $this->getRoleId();
+        } catch (Exception $e) {
+            return $this->line("Error user specified is invalid");
+        }
+
+        try {
+            RoleMember::create(['user_id' => $user_id, 'role_id' => $role_id]);
+        } catch (Exception $e) {
+            \Log::debug($e->getMessage());
+            $this->line("Unable to add user to role");
+        }
 
         /* Get list of roles */
+        $this->line("User {$user_id} added to role {$role_id}");
     }
 
     /**
@@ -72,6 +90,13 @@ class RoleAssignCommand extends Command
         ];
     }
 
+    /**
+     * Builds the userlist into an array suitable for tabular output.
+     *
+     * @param $users
+     *
+     * @return array
+     */
     public function buildUserTable($users)
     {
         $user_data = [];
@@ -80,5 +105,50 @@ class RoleAssignCommand extends Command
             $user_data[] = [$user->id, $user->name];
         }
         return $user_data;
+    }
+
+    /**
+     * Get the user id.
+     * @return string
+     */
+    public function getUserId()
+    {
+        /* Get list of users */
+        $users = User::all();
+        $userData = $this->buildUserTable($users);
+        $this->table(['id', 'username'], $userData);
+
+        $user_id = $this->ask('Which user would you like to assign a role for? [1,2,...]');
+
+        return $user_id;
+    }
+
+    /**
+     * Build Roles table.
+     * @param $roles
+     * @return array
+     */
+    public function buildRoleTable($roles)
+    {
+        $role_data = [];
+        foreach($roles as $r)
+        {
+            $role_data[] = [$r->id, $r->name];
+        }
+        return $role_data;
+    }
+
+    /**
+     * Get the role id
+     * @return integer $role_id
+     */
+    public function getRoleId()
+    {
+        $roles = Role::all();
+        $roleData = $this->buildRoleTable($roles);
+        $this->table(['id', 'role'], $roleData);
+
+        $role_id = $this->ask('Which role would you like to assign to the user? [1,2,...]');
+        return $role_id;
     }
 }
