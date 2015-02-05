a2f7ec2c4d2f5dda879784fc87c62e870be2cac4
Taskforcedev <taskforce2eu@gmail.com>
2015-02-05 12:34:39 +0000

    Pass user through to view.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,39 +1,43 @@
-<?php namespace DiabloDB\Http\Controllers;
+<?php
 
-class HomeController extends Controller {
+namespace DiabloDB\Http\Controllers;
 
-	/*
-	|--------------------------------------------------------------------------
-	| Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| This controller renders your application's "dashboard" for users that
-	| are authenticated. Of course, you are free to change or remove the
-	| controller as you wish. It is just here to get your app started!
-	|
-	*/
+use \Auth;
 
-	/**
-	 * Create a new controller instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		$this->middleware('auth');
-	}
+class HomeController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Home Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller renders your application's "dashboard" for users that
+    | are authenticated. Of course, you are free to change or remove the
+    | controller as you wish. It is just here to get your app started!
+    |
+    */
 
-	/**
-	 * Show the application dashboard to the user.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard to the user.
+     *
+     * @return Response
+     */
+    public function index()
+    {
         $data = [
-            'sitename' => \Config::get('diablo.sitename')
+            'sitename' => \Config::get('diablo.sitename'),
+            'user' => Auth::user()
         ];
-		return view('home', $data);
-	}
-
+        return view('home', $data);
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,16 +3,19 @@
 @section('content')
 
 <div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Home</div>
 
-				<div class="panel-body">
-					You are logged in!
-				</div>
-			</div>
-		</div>
-	</div>
+                <div class="panel-body">
+                    Welcome {{ $user->name }}!
+                    @if($user->isAdmin())
+                        You are an admin!
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
