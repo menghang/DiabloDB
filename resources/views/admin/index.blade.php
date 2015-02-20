951befa6df9179d61169f122a7ea4d16ab4db0ee
Taskforcedev <taskforce2eu@gmail.com>
2015-02-20 16:27:32 +0000

    Add admin index page.

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts/master')
+
+@section('content')
+
+    <h1>Administration</h1>
+
+    <h2>Battletags</h2>
+    If you have entered any battletags they will show here.
+
+    <h2>Users</h2>
+
+@endsection
