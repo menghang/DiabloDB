4c6d6512f416304d4f8131944e14a2774e050c83
Taskforcedev <taskforce2eu@gmail.com>
2015-02-20 16:17:09 +0000

    Add admin route.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,8 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
+Route::get('admin', 'AdminController@index');
+
 Route::get('login', 'Auth\AuthController@getLogin');
 Route::get('logout', 'Auth\AuthController@getLogout');
 Route::get('register', 'Auth\AuthController@getRegister');
