a92640bc9450f5da6976c95d3fe9930cec6e4d96
Taskforcedev <taskforce2eu@gmail.com>
2015-02-20 15:56:09 +0000

    Update wording on welcome page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,10 +7,9 @@
 It appears you may have just installed the Diablo 3 Database Application.  Please follow the steps outlined below to get started.
 
 <h2>Important!</h2>
-As you have just registered your account, there 
+If you have just registered your account please make sure you run the role assign command to grant yourself administrative privileges.
 
-<h2>Configuration</h2>
-
-If you would like to automatically poll the diablo api to retrieve characters then you should enter your clan and api details in config/diablo.php.  As well as automatic you can also manually edit characters although this is generally not recommended.
+<h3>Character Listing</h3>
+Once you have created at least one profile and clicked update, you should see this screen becomes replaced with a character listing.
 
 @endsection
