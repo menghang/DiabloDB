e9d4711cc6a1a367ef0543928ad7f8d2d8889798
Taskforcedev <taskforce2eu@gmail.com>
2015-02-23 11:02:54 +0000

    If members isset show members as table, else show message.

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,12 +1,24 @@
+<?php
+function actions($id)
+{
+    echo '<button class="btn btn-info">Edit</button> <button class="btn btn-danger">Delete!</button>';
+}
+?>
+
 @extends('layouts/master')
 
 @section('content')
-
     <h1>Administration</h1>
 
-    <h2>Battletags</h2>
-    If you have entered any battletags they will show here.
-
-    <h2>Users</h2>
+    <h2>Members<span class="pull-right"><button class="btn btn-primary">Add User</button></span></h2>
+    @if(isset($members) && !empty($members))
+        <table class="table">
+        @foreach($members as $member)
+            <tr><td>{{ $member->name }}</td><td>{{ $member->battletag }}</td><td>{{ actions($member->id) }}</td></tr>
+        @endforeach
+        </table>
+    @else
+        You do not have any users at present, please use the create button to add some.
+    @endif
 
 @endsection
