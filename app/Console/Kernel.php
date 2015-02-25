274282ac07a589e85006d98fee126b9ab5c64cd1
Taskforcedev <taskforce2eu@gmail.com>
2015-02-25 12:24:01 +0000

    Configure schedule frequency in Kernel.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,10 +4,14 @@ namespace DiabloDB\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use Illuminate\Support\Facades\Config;
 
 class Kernel extends ConsoleKernel
 {
+    public $updateFreqency = [
+        'characters' => 'hourly',
+        'members' => 'daily'
+    ];
+
     /**
      * The Artisan commands provided by your application.
      *
@@ -16,12 +20,15 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         'DiabloDB\Console\Commands\RoleAssignCommand',
         'DiabloDB\Console\Commands\CharactersUpdateCommand',
+        'DiabloDB\Console\Commands\MembersUpdateCommand',
+        'DiabloDB\Console\Commands\CharacterUpdateCommand',
+        'DiabloDB\Console\Commands\MemberUpdateCommand',
     ];
 
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
@@ -32,21 +39,17 @@ class Kernel extends ConsoleKernel
         ];
 
         /* Update Members */
-        $memberSchedule = \Config::get('diablo.schedule.members');
+        $memberSchedule = $this->updateFreqency['members'];
         if (in_array($memberSchedule, $validMethods)) {
             $schedule->command('MembersUpdateCommand')
                 ->$memberSchedule();
         }
 
         /* Update Characters */
-        $characterSchedule = \Config::get('diablo.schedule.characters');
+        $characterSchedule = $this->updateFreqency['characters'];
         if (in_array($characterSchedule, $validMethods)) {
             $schedule->command('CharactersUpdateCommand')
                 ->$characterSchedule();
         }
-
-        //$schedule->command('inspire')
-        //         ->hourly();
     }
-
 }
