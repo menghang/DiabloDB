e5f97533c40e4e0033771f6d68ac7bbf3010bbbc
Taskforcedev <taskforce2eu@gmail.com>
2015-03-02 14:03:00 +0000

    Placeholder command for character updating.

---
+++ app/Commands/CharacterUpdate.php
@@ -0,0 +1,28 @@
+<?php namespace DiabloDB\Commands;
+
+use DiabloDB\Commands\Command;
+
+use Illuminate\Contracts\Bus\SelfHandling;
+
+class CharacterUpdate extends Command implements SelfHandling {
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		//
+	}
+
+	/**
+	 * Execute the command.
+	 *
+	 * @return void
+	 */
+	public function handle()
+	{
+
+	}
+}
