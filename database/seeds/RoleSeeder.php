25598b53c5fe2b41a17597227bf113862a7ee5bd
Taskforcedev <taskforce2eu@gmail.com>
2015-03-02 14:17:20 +0000

    Add Role seeder.

--- database/seeds/RoleSeeder.php
+++ database/seeds/RoleSeeder.php
@@ -1,6 +1,15 @@
 <?php
 
+use DiabloDB\Role;
+
 class RoleSeeder
 {
+    public $roles = ['Administrator', 'Member'];
 
+    public function run()
+    {
+        foreach ($this->roles as $role) {
+            Role::create(['name' => $role]);
+        }
+    }
 }
