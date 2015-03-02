2557bcbfcb51700c8ed18bc405a7baf0242e9865
Taskforcedev <taskforce2eu@gmail.com>
2015-03-02 14:02:39 +0000

    Add MemberUpdate command.

---
+++ app/Commands/MemberUpdate.php
@@ -0,0 +1,46 @@
+<?php namespace DiabloDB\Commands;
+
+use DiabloDB\Commands\Command;
+use DiabloDB\Battlenet\Api;
+use DiabloDB\Member;
+use DiabloDB\Character;
+use Illuminate\Contracts\Bus\SelfHandling;
+
+class MemberUpdate extends Command implements SelfHandling
+{
+    private $member;
+
+    /**
+     * Create a new command instance.
+     * @param Member $member
+     */
+    public function __construct(Member $member)
+    {
+        $this->member = $member;
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $api = new Api();
+        $member = $this->member;
+        $data = $api->getProfileInfo($member->battletag);
+
+        /* Update member paragon levels */
+        $member->paragon = $data['paragonLevel'];
+        $member->paragon_hc = $data['paragonLevelHardcore'];
+        $member->paragon_curr_season = $data['paragonLevelSeason'];
+        $member->paragon_curr_season_hc = $data['paragonLevelSeasonHardcore'];
+        $member->save();
+
+        /* Update members characters */
+        $characters = $data['heroes'];
+        foreach($characters as $char) {
+            Character::CreateOrUpdate($char);
+        }
+    }
+}
