823b8995cf8a3a991f6bd7547415ec1fe336cedb
Taskforcedev <taskforce2eu@gmail.com>
2015-03-02 17:00:36 +0000

    Add dropdown for admins.

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -22,7 +22,13 @@
                         <li><a href="/auth/register">Register</a></li>
                     @else
                         @if (isset($user) && $user->isAdmin())
-                            <li><a href="admin">Admin</a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Admin <span class="caret"></span></a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="/admin">Dashboard</a></li>
+                                    <li><a href="/admin/members">Members</a></li>
+                                </ul>
+                            </li>
                         @endif
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
